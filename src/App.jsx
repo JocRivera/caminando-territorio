2aee08562b9dca0ed72280ce49c0afde61f705df
JocRivera <jose_mrivera@soy.sena.edu.co>
2024-09-20 16:21:19 -0500

    deleteSidebar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Map from './components/map'
 import NavScrollExample from './components/layouts/Navbar'
-import SidebarExample from './components/layouts/Sidebar'
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -15,14 +14,7 @@ function App() {
     <>
       <NavScrollExample />
       <div className='container-fluid mt-3'>
-        <div className='row'>
-          <div className='col-md-3'>
-            <SidebarExample />
-          </div>
-          <div className='col-md-9'>
-            <Map />
-          </div>
-        </div>
+        <Map />
       </div>
     </>
   )
